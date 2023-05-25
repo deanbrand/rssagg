aea810ee2f0b405872258ac627846ab42c8f9331
deanbrand <deanbrand@protonmail.com>
2023-05-25 15:08:51 +0200

    boilerplate for http server complete

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerError)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Println("Listening on port " + portString)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
