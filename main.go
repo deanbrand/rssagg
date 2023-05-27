a75c9a39a098dc90e4c6bc8c4f900ff127c9cf58
deanbrand <deanbrand@protonmail.com>
2023-05-27 13:55:00 +0200

    posts functionality

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/deanbrand/rssagg/internal/database"
 	"github.com/go-chi/chi"
@@ -18,7 +19,6 @@ type apiConfig struct {
 }
 
 func main() {
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
@@ -36,9 +36,12 @@ func main() {
 		log.Fatal("Can't connect to database: ", err)
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
@@ -59,6 +62,8 @@ func main() {
 
 	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+
+	v1Router.Get("/posts", apiCfg.middlewareAuth(apiCfg.handlerGetPostsForUser))
 
 	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
 	v1Router.Get("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollows))
---
+++ scraper.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/deanbrand/rssagg/internal/database"
+	"github.com/google/uuid"
+)
+
+func startScraping(db *database.Queries, concurrency int, timeBetweenRequest time.Duration) {
+	log.Printf("Scraping on %v goroutines every %s duration", concurrency, timeBetweenRequest)
+	ticker := time.NewTicker(timeBetweenRequest)
+
+	for ; ; <-ticker.C {
+		feeds, err := db.GetNextFeedsToFetch(
+			context.Background(),
+			int32(concurrency),
+		)
+		if err != nil {
+			log.Printf("Error getting feeds to fetch: %v", err)
+			continue
+		}
+
+		wg := &sync.WaitGroup{}
+		for _, feed := range feeds {
+			wg.Add(1)
+
+			go scrapeFeed(db, wg, feed)
+		}
+		wg.Wait()
+	}
+}
+
+func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
+	defer wg.Done()
+
+	_, err := db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Printf("Error marking feed as fetched: %v", err)
+		return
+	}
+
+	rssFeed, err := urlToFeed(feed.Url)
+	if err != nil {
+		log.Printf("Error getting feed: %v", err)
+		return
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Error parsing time %v: %v", item.PubDate, err)
+			continue
+		}
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description,
+			PublishedAt: pubAt,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			if strings.Contains(err.Error(), "unique constraint") {
+				continue
+			}
+			log.Printf("Error creating post: %v", err)
+			continue
+		}
+	}
+	log.Printf("Feed: %s collected, %v items", feed.Name, len(rssFeed.Channel.Item))
+}
